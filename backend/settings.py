b8776b5903887da3f7addd50f02b67c9e8b4f978
Shahbaz Alam <sbzalam2024@gmail.com>
2025-10-24 23:55:28 +0530

    revert to sqlite database

--- backend/settings.py
+++ backend/settings.py
@@ -78,22 +78,23 @@ ASGI_APPLICATION = 'backend.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'real-time-chat-db',
-        'USER': 'postgres',
-        'PASSWORD': 'pass@123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'real-time-chat-db.sqlite3',  # 👈 changed database file name
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'real-time-chat-db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'pass@123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
