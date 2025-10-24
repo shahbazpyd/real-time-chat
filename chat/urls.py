4d9eff9d888c28cb753e151d72ff87c94b60f342
Shahbaz Alam <sbzalam2024@gmail.com>
2025-10-24 15:36:20 +0530

    configured react

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('rooms/', views.ChatRoomList.as_view(), name='chatroom-list'),
     # The <slug:room_slug> part captures the slug from the URL
     path('rooms/<slug:room_slug>/messages/', views.MessageList.as_view(), name='message-list'),
-]
+] 
